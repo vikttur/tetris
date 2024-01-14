e5960e5273312b44df777b7254a59eb9b2333485
vikttur <vikttur.job@gmail.com>
2024-01-15 01:05:42 +0200

    transfers constants to a separate file

--- js/main.js
+++ js/main.js
@@ -1,44 +1,4 @@
-const PLAYFIELD_COLUMNS = 10;
-const PLAYFIELD_ROWS = 20;
-
-const FIGURES = {
-	'O': [
-		[1, 1],
-		[1, 1],
-	],
-	'T': [
-		[0, 0, 0],
-		[1, 1, 1],
-		[0, 1, 0],
-	],
-	'L': [
-		[0, 0, 0],
-		[1, 1, 1],
-		[1, 0, 0],
-	],
-	'Lr': [
-		[0, 0, 0],
-		[1, 1, 1],
-		[0, 0, 1],
-	],
-	'S': [
-		[0, 0, 0],
-		[0, 1, 1],
-		[1, 1, 0],
-	],
-	'Sr': [
-		[0, 0, 0],
-		[1, 1, 0],
-		[0, 1, 1],
-	],
-	'I': [
-		[0, 1, 0, 0],
-		[0, 1, 0, 0],
-		[0, 1, 0, 0],
-		[0, 1, 0, 0],
-	],
-};
-
+import { 	PLAYFIELD_COLUMNS, PLAYFIELD_ROWS, FIGURES } from './constants.js';
 
 const figureNames = Object.keys(FIGURES);
 
