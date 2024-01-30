da610901b621ac5f3d35f12ea6c386081c1995bd
vikttur <vikttur.job@gmail.com>
2024-01-30 23:49:24 +0200

    separates figure rotation check functions

--- js/rotateFigure/isPermissionToRotate.js
+++ js/rotateFigure/isPermissionToRotate.js
@@ -4,6 +4,16 @@ import { isExitFromFieldToDown, isExitFromFieldToSide, isOverlayingFigures } fro
 function isPermissionToRotate(figure) {
 	const { size, row, column } = figure;
 	
+  if(!checkingExitToDown(figure)) return; 
+  if(!checkingExitToSide(figure)) return; 
+	if(isOverlayingFigures(0, 0)) return;
+
+	return true;
+}
+
+function checkingExitToDown(figure) {
+  const { size, row } = figure;
+
 	for(let i = 0; i < size; i += 1) {
 		if(i + row < 0) break;
 
@@ -12,15 +22,19 @@ function isPermissionToRotate(figure) {
 		}
 	}
 
+  return true;
+}
+
+function checkingExitToSide(figure) {
+  const { size, column } = figure;
+
 	for(let j = 0; j < size; j += 1){
 		if(column + j < 0 || column + j >= PLAYFIELD_COLUMNS) {
 			if(isExitFromFieldToSide(j)) return;
 		}
 	}
 
-	if(isOverlayingFigures(0, 0)) return;
-
-	return true;
+  return true;
 }
 
 export {
