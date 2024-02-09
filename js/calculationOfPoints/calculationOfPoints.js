044f0cef7a4abb3e6a2077ed7da76baf29e2a67e
vikttur <vikttur.job@gmail.com>
2024-02-09 23:24:08 +0200

    adds a result block

--- js/calculationOfPoints/calculationOfPoints.js
+++ js/calculationOfPoints/calculationOfPoints.js
@@ -4,7 +4,7 @@ let pointsSum = 0;
 
 function calculationOfPoints (points) {
   pointsSum += points;
-  document.querySelector('.balls').innerHTML = pointsSum;
+  document.querySelector('.score').innerHTML = pointsSum;
   savePoints(pointsSum);
 }
 
--- js/calculationOfPoints/pointsForFigures.js
+++ js/calculationOfPoints/pointsForFigures.js
@@ -2,8 +2,10 @@ import { calculationOfPoints } from './calculationOfPoints.js';
 
 function pointsForFigures() {
   calculationOfPoints(1);
+  calculationOfFigures(1);
 }
 
 export {
   pointsForFigures,
 }
+
